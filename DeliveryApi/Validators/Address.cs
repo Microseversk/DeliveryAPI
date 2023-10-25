6e9181638d3984638431509976addd2ff2b0343d
Otrijka <122968877+Otrijka@users.noreply.github.com>
2023-10-25 16:30:29 +0700

    [iss14]Валидация пароля и адреса

---
+++ DeliveryApi/Validators/Address.cs
@@ -0,0 +1,14 @@
+﻿using DeliveryApi.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryApi.Validators;
+
+public static class Address
+{
+    public async static Task<bool> Isvalid(AddressContext context, Guid? addressId)
+    {
+        var addressValid = (await context.AsAddrObjs.FirstOrDefaultAsync(a => a.Objectguid == addressId) != null) ||
+                           (await context.AsHouses.FirstOrDefaultAsync(h => h.Objectguid == addressId) != null);
+        return addressValid;
+    }
+}
