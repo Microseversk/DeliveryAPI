1511b8105de4a0856dad718550c87092359a5eb9
Otrijka <122968877+Otrijka@users.noreply.github.com>
2023-11-08 18:53:06 +0700

    fix: добавил проверку актуальности адреса

--- DeliveryApi/Validators/Address.cs
+++ DeliveryApi/Validators/Address.cs
@@ -7,8 +7,8 @@ public static class Address
 {
     public async static Task<bool> Isvalid(AddressContext context, Guid? addressId)
     {
-        var addressValid = (await context.AsAddrObjs.FirstOrDefaultAsync(a => a.Objectguid == addressId) != null) ||
-                           (await context.AsHouses.FirstOrDefaultAsync(h => h.Objectguid == addressId) != null);
+        var addressValid = (await context.AsAddrObjs.FirstOrDefaultAsync(a => a.Objectguid == addressId && a.Isactual == 1) != null) ||
+                           (await context.AsHouses.FirstOrDefaultAsync(h => h.Objectguid == addressId && h.Isactual == 1) != null);
         return addressValid;
     }
 }
